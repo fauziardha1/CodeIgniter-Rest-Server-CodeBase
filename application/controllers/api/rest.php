97582dcfc6c0f6c26e0df00f972de85a16556a5a
emryswarlock <ardha275@gmail.com>
2020-09-11 14:33:57 +0700

    menambahkan fitur midtrans payment

---
+++ application/controllers/api/rest.php
@@ -0,0 +1,34 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Sarugo extends REST_Controller{
+    public function index_get()
+    {
+        $data = "hello World!";
+
+        $this->response($data,200);
+        // $this->load->view("welcome_message");
+    }
+
+    public function index_post()
+    {
+        $data = "hello World! post";
+
+        $this->response($data,200);
+    }
+    public function index_put()
+    {
+        $data = "hello World! put";
+
+        $this->response($data,200);
+    }
+    public function index_delete()
+    {
+        $data = "hello World! delete";
+
+        $this->response($data,200);
+    }
+}
